5a0cbd7f1d2189d021c8517770e51a544448f368
Piyush Patle <patlepiyush382@gmail.com>
2024-12-28 00:24:36 +0530

    Local to git repo

---
+++ SnakeGame.java
@@ -0,0 +1,14 @@
+package snakeGame;
+
+import javax.swing.*;
+
+public class SnakeGame extends JFrame{
+
+    JFrame frame = new JFrame();
+    SnakeGame(){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
